89805a77600643076bd9795acd147e84c27bf863
Max David <me@maxdavid.com>
2013-07-25 15:26:42 -0700

    Renaming TARGET_IP to DEST_IP

--- scp_transfer.sh
+++ scp_transfer.sh
@@ -2,9 +2,9 @@
 # 
 # SCP a file to a given IP. user:pass and target dir are defined below.
 #
-# usage: ./scp_transfer.sh <target_ip>
-#   $1 = target IP address
-#   $2 = file to be transferred
+# usage: ./scp_transfer.sh <transfer_file> [<dest_ip>]
+#   $1 = file to be transferred
+#   $2 = destination IP address (optional if defined below)
 #
 
 # directory where sized transfer files are stored
@@ -16,9 +16,9 @@ REMOTE_PASS='Partytime1'
 # location on target IP to transfer files to
 REMOTE_DIR='/dev/null'
 
-TARGET_IP=$1
-FILENAME=$2
+FILENAME=$1
+DEST_IP=${2:-"129.170.213.70/32"} # define default destination ip here
 
 
-sshpass -p $REMOTE_PASS $FILENAME $REMOTE_USER@$TARGET_IP:$REMOTE_DIR
+sshpass -p $REMOTE_PASS $FILENAME $REMOTE_USER@$DEST_IP:$REMOTE_DIR
 
